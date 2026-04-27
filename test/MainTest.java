20e811a2fa81a08fb7e8325959601e8ca0394928
Talshin <a.svazy1245@gmail.com>
2026-04-27 09:50:06 +0500

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,15 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
-public class MainTest {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    @Test
-    void testSum() {
-        assertEquals(5, Main.sum(2, 3));
-    }
+        // 1. Считываем размер массива
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            int[] array = new int[n];
+            int countZeros = 0;
 
-    @Test
-    void testNegative() {
-        assertEquals(-1, Main.sum(2, -3));
+            // 2. Считываем элементы массива и сразу проверяем на ноль
+            for (int i = 0; i < n; i++) {
+                array[i] = scanner.nextInt();
+                if (array[i] == 0) {
+                    countZeros++;
+                }
+            }
+
+            // 3. Выводим результат
+            System.out.println(countZeros);
+        }
     }
 }
